63c3e0bccf3ebb8eefcb1973448d8dcbf78438aa
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-09 15:25:01 -0800

    completed validation assignment without the last sensei bonus

--- apps/main_app/models.py
+++ apps/main_app/models.py
@@ -1,5 +1,21 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import date, datetime
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title name should be at least 2 characters long"
+        if Show.objects.filter(title = postData['title']).exists():
+            errors['title'] = "This TV show already exists"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network name should be at least 3 characters long"
+        if len(postData['description']) < 10 and len(postData['description']) > 0:
+            errors['desc'] = "The description should be at least 10 characters long"
+        if len(postData['release_date']) == 0 or datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.today():
+            errors['release_date'] = "The release date should be a valid date and in the past"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -8,5 +24,7 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
     def __repr__(self):
         return f"<Show Object: {self.id} {self.title} {self.network} {self.release_date}>"
--- apps/main_app/views.py
+++ apps/main_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
-from .models import Show
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
+from django.contrib import messages
+
 
 def home_page(request):
-  return redirect('/shows')
+    return redirect('/shows')
+
 
 def index(request):
     context = {
@@ -16,32 +19,49 @@ def new(request):
 
 
 def create(request):
-    new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['description'])
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, values in errors.items():
+            messages.error(request, values)
+        return redirect('/shows/new')
+    else:
+        new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'],
+                                       release_date=request.POST['release_date'], desc=request.POST['description'])
+        messages.success(request, "New show successfully updated")
+        return redirect(f"/shows/{new_show.id}")
 
-    return redirect(f"/shows/{new_show.id}")
 
 def show_profile(request, show_id):
-  context = {
-    'show': Show.objects.get(id=show_id),
-  }
-  return render(request, 'main_app/show_profile.html', context)
+    context = {
+        'show': Show.objects.get(id=show_id),
+    }
+    return render(request, 'main_app/show_profile.html', context)
+
 
 def edit_profile(request, show_id):
-    context={
-      'show': Show.objects.get(id=show_id),
+    context = {
+        'show': Show.objects.get(id=show_id),
     }
     return render(request, 'main_app/edit_profile.html', context)
 
+
 def update_profile(request, show_id):
-    show = Show.objects.get(id=show_id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.release_date = request.POST['release_date']
-    show.desc = request.POST['description']
-    show.save()
-    return redirect(f"/shows/{show_id}")
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, values in errors.items():
+            messages.error(request, values)
+        return redirect(f"/shows/{show_id}/edit")
+    else:
+        show = Show.objects.get(id=show_id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['release_date']
+        show.desc = request.POST['description']
+        show.save()
+        return redirect(f"/shows/{show_id}")
+
 
 def destroy_profile(reqest, show_id):
-    show=Show.objects.get(id=show_id)
+    show = Show.objects.get(id=show_id)
     show.delete()
     return redirect('/shows')
